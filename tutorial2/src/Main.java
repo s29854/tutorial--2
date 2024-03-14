c08a38ee6c3c1385fabbf2515fd4d683027ea2f6
s29854 <s29854@pjwstk.edu.pl>
2024-03-14 14:21:56 +0100

    prawidlowo dzialajaca aplikacja

--- tutorial2/src/Main.java
+++ tutorial2/src/Main.java
@@ -3,30 +3,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate.now().getDayOfWeek().name();
-            LocalDate today =  LocalDate.now();
-            System.out.println(today.getDayOfWeek());
 
-            Scanner scanner = new Scanner(System.in);
-            double price = 40;
-            System.out.println("Podaj wiek:");
-            int age = scanner.nextInt();
+            int age = getAgeFromTheUser();
+            String city = getCityFromTheUser();
+            double discount = discountCalculator(age, city);
+            int price = (int) priceCalculator(discount);
 
-            System.out.println("W jakim miescie mieszkasz?");
-            String city = scanner.next();
-
-            double discount = 0;
-            if (age < 10 || LocalDate.now().getDayOfWeek().name().equals("THURSDAY")) {
-                price = 0;
-            } else if (10 <= age && age < 18) {
-                discount = discount + 0.5;
-                price = price*discount;
-            }
-            if (city.equals("Warszawa")) {
-                discount = 0;
-
-            }
-            double discountedPrice = price*discount;
-            System.out.println("Data: " +city+", "+age+" years old, " );
+            System.out.println("Data: " +city+ ", " +age+ " years old, "
+                    +LocalDate.now().getDayOfWeek().name().toLowerCase()+
+                    "\nTicket price: " +price+ " PLN"+
+                    "\nDiscount: " +Math.round((1 - discount)*100)+ "%");
         }
-    }
+    public static int getAgeFromTheUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoj wiek:");
+        int age = scanner.nextInt();
+
+        return age;}
+
+    public static String getCityFromTheUser(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("W jakim miescie mieszkasz?");
+        String city = scanner.next();
+
+        return city;}
+
+    public static double discountCalculator (int age, String city){
+        double discount = 1;
+        if (age < 10){
+            discount *= 0;
+        }
+        else if (age <= 18){
+            discount *= 0.5;
+        }
+        if (city.equalsIgnoreCase("warszawa")){
+            discount *= 0.9;}
+        if (LocalDate.now().getDayOfWeek().name().equalsIgnoreCase("tHURSDAY")){
+            discount *= 0;
+        }
+    return discount;}
+
+    public static double priceCalculator(double discount){
+       double price = 40*discount;
+    return price;}
+
+
+
+}
